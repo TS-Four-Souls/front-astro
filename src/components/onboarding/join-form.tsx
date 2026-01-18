9d31f71ab0169ef6796207a72f20785bb6a96d7e
DrMint <29893320+DrMint@users.noreply.github.com>
2026-01-18 10:39:28 +0100

    Improve styling of game onboarding

--- src/components/onboarding/join-form.tsx
+++ src/components/onboarding/join-form.tsx
@@ -9,7 +9,8 @@ interface JoinFormProps {
 export const JoinForm = ({ onJoin }: JoinFormProps) => {
   const [name, setName] = useState("");
 
-  const joinGame = async () => {
+  const joinGame = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     console.log("joining game with name", name);
     socket.emit("join", name, (response) => {
       if (response.status === 200) {
@@ -21,16 +22,23 @@ export const JoinForm = ({ onJoin }: JoinFormProps) => {
   };
 
   return (
-    <div>
-      <h1>Join the game</h1>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        type="text"
-        placeholder="Enter your name"
-        required
-      />
-      <button onClick={joinGame}>Join</button>
+    <div className="grid place-items-center place-content-center h-screen">
+      <div className="flex flex-col gap-4 border-2 border-stone-700 p-4 rounded-lg text-center">
+        <h1 className="text-xl font-bold">Join the game</h1>
+        <form onSubmit={joinGame} className="flex flex-col gap-2">
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Enter your name..."
+            autoComplete="off"
+            minLength={1}
+            required
+            className="bg-stone-800 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-stone-500"
+          />
+          <button className="bg-stone-600 text-white px-4 py-2 rounded-md hover:bg-stone-500 transition-colors cursor-pointer" type="submit">Join</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/onboarding/start-step.tsx
+++ src/components/onboarding/start-step.tsx
@@ -14,9 +14,12 @@ export const StartStep = ({ issuer }: StartStepProps) => {
     });
   };
   return (
-    <div>
-      <h1>Start step</h1>
-      <button onClick={requestStart}>Start</button>
+    <div className="grid place-items-center place-content-center h-screen">
+      <div className="flex flex-col gap-8 border-2 border-stone-700 p-8 rounded-lg text-center">
+        <h1 className="text-xl font-bold">Welcome {issuer.id}!</h1>
+        <p className="text-sm text-stone-400">When everyone is ready,<br/>click the button below to start the game</p>
+        <button className="bg-stone-600 text-white px-4 py-2 rounded-md hover:bg-stone-500 transition-colors cursor-pointer" onClick={requestStart}>Start</button>
+      </div>
     </div>
   );
 };
