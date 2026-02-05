f482343755bd23d1fe0987af5d71563563bc53ab
DrMint <29893320+DrMint@users.noreply.github.com>
2026-02-05 01:25:01 +0100

    Nicer loading

--- src/components/onboarding/loading.tsx
+++ src/components/onboarding/loading.tsx
@@ -1,8 +1,17 @@
 export const Loading = () => {
   return (
-    <div className="grid h-screen place-content-center place-items-center">
-      <div className="flex flex-col gap-4 rounded-lg border-2 border-stone-700 p-4 text-center">
-        <h1 className="text-xl font-bold">Loading...</h1>
+    <div className="flex h-screen gap-8">
+      <div className="planetarium flex flex-2 flex-col">
+        <div className="flex flex-1 place-content-center place-items-center">
+          <img src="/logo.png" alt="Logo" className="mb-16a w-[30vw]" />
+        </div>
+        <p className="mx-16 mb-4 text-center font-alt-stats text-blue-200/60">
+          This is an unofficial, fan-made website and is not affiliated with
+          <br /> or endorsed by Maestro Media and Edmund McMillen.
+        </p>
+      </div>
+      <div className="flex flex-1 flex-col place-content-center place-items-center gap-8 font-main text-3xl">
+        Loading...
       </div>
     </div>
   );
