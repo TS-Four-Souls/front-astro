8fe5c9b04aefbe8e489c17f7b605ca21095e98e6
DrMint <29893320+DrMint@users.noreply.github.com>
2026-04-18 09:38:54 +0200

    fix: removed debug page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,24 +1,19 @@
 import { PromptProvider } from "./board/contexts/prompt-context";
 import { ToastProvider } from "./board/contexts/toast-context";
-import { DebugPage } from "./pages/debug-page";
 import { GamePage } from "./pages/game-page";
 import { UserSettingsProvider } from "./board/contexts/user-settings-context";
 import { HotkeysProvider } from "react-hotkeys-hook";
 import { HotkeyScope } from "@/utils/hotkey";
 import { PopoverProvider } from "./board/contexts/popover-context";
 
-interface AppProps {
-  page: "debug" | "game";
-}
-
-export const App = ({ page }: AppProps) => (
+export const App = () => (
   <div className="min-h-screen w-screen overflow-hidden bg-stone-800 text-white">
     <HotkeysProvider initiallyActiveScopes={[HotkeyScope.Main]}>
       <PopoverProvider>
         <UserSettingsProvider>
           <ToastProvider>
             <PromptProvider>
-              {page === "debug" ? <DebugPage /> : <GamePage />}
+              <GamePage />
             </PromptProvider>
           </ToastProvider>
         </UserSettingsProvider>
