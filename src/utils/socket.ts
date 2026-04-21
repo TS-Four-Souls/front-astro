716caa3a3db51529c1501c36f1177b6c9fbb99b0
slichau <sylvain.lichau@inria.fr>
2026-04-21 13:17:33 +0200

    autoconnect delay

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,11 +2,18 @@ import { BASE_URL, SERVER_API_KEY } from "astro:env/client";
 import { io, type Socket } from "socket.io-client";
 import type { ClientToServerEvents, ServerToClientEvents } from "../shared/api";
 
+const isBrowser = typeof window !== "undefined";
+
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   BASE_URL,
   {
     auth: {
       apiKey: SERVER_API_KEY,
     },
+    autoConnect: isBrowser,
+    transports: ["websocket"],
+    reconnectionDelay: 500,
+    reconnectionDelayMax: 5000,
+    timeout: 10000,
   },
 );
