1dd8507aa4ee39b1031d6f8c076733d01d18e031
slichau <sylvain.lichau@inria.fr>
2026-06-15 17:10:03 +0200

    fix: typo

--- src/components/board/center.tsx
+++ src/components/board/center.tsx
@@ -137,7 +137,7 @@ export const Center = ({ state }: CenterProps) => {
                     tooltip={{
                       enabled: true,
                       title: "Bonus soul",
-                      content: "Met the conditions to receive this bonus soul.",
+                      content: "Meet the conditions to receive this bonus soul.",
                     }}
                   />
                 )}
