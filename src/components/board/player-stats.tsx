e8de8b1b12685677d1840d989821257d01625e38
slichau <sylvain.lichau@inria.fr>
2026-06-30 10:24:29 +0200

    feat: round counter in the player hot bar

--- src/components/board/player-stats.tsx
+++ src/components/board/player-stats.tsx
@@ -15,6 +15,7 @@ import type { Player, PlayerMe } from "@/shared/api";
 import { TeamIcon } from "@/icons/team-icon";
 import { useHotkeys } from "react-hotkeys-hook";
 import { HotkeyScope } from "@/utils/hotkey";
+import { RoundCounter } from "@/icons/RoundCounter";
 
 interface PlayerStatsProps {
   player: Player | PlayerMe;
@@ -264,6 +265,8 @@ export const PlayerStats = ({ player, className }: PlayerStatsProps) => {
 
       {isMe && (
         <div className="flex items-center gap-4">
+          <RoundCounter
+          value={state.round}></RoundCounter>
           <Button
             className="shadow-lg shadow-taupe-800/70"
             disabled={state.me.capabilities.endTurn !== true}
