a0627b46c4873a70ca12775f97ad1fda9d219537
DrMint <29893320+DrMint@users.noreply.github.com>
2026-08-02 13:09:10 +0200

    fix: button no longer shrink

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -39,7 +39,7 @@ export const Button = ({
   return (
     <button
       className={cn(
-        "relative overflow-hidden flex place-content-center place-items-center gap-2 rounded-md px-4 py-2 font-main text-white uppercase shadow-2xl inset-shadow-xs shadow-taupe-950/10 inset-shadow-taupe-100/10 transition-[colors,filter]",
+        "relative overflow-hidden shrink-0 flex place-content-center place-items-center gap-2 rounded-md px-4 py-2 font-main text-white uppercase shadow-2xl inset-shadow-xs shadow-taupe-950/10 inset-shadow-taupe-100/10 transition-[colors,filter]",
         theme === "default" && "bg-taupe-600",
         theme === "onLight" && "bg-taupe-500",
         theme === "onDark" && "bg-taupe-700",
